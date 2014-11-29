72bf5b9565b276ead5f9b0b9e6d6c9bd97405eda
clarkb7 <brandenjclark+github@gmail.com>
2014-11-29 15:22:16 -0500

    Fixes no screensaver script sleep time

--- scripts/no_screensaver.sh
+++ scripts/no_screensaver.sh
@@ -2,5 +2,5 @@
 # xscreensaver is not detecting full-screen applications, so I run this to prevent
 #   the screensaver from activating during movies.
 
-while true; do xscreensaver-command -deactivate; sleep 10;done
+while true; do xscreensaver-command -deactivate; sleep 10m;done
 
