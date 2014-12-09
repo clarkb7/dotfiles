dd7a4ed58f646e9f9fc8e4ffb412080a690da0fd
clarkb7 <brandenjclark+github@gmail.com>
2014-12-09 15:59:08 -0500

    Organize xmonad.hs a little

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,17 +1,20 @@
+-- XMonad imports
 import XMonad
 import qualified Data.Map as M
-import XMonad.Hooks.DynamicLog
+import qualified XMonad.StackSet as W
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Util.EZConfig
+import XMonad.Util.Themes
 import XMonad.Actions.CycleWS
+import XMonad.Actions.SpawnOn
+-- Hooks
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+-- Layouts
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.ResizableTile
-import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
-import XMonad.Hooks.ManageDocks
-import qualified XMonad.StackSet as W
-import XMonad.Actions.SpawnOn
-import XMonad.Util.Themes
 import XMonad.Layout.SubLayouts
 import XMonad.Layout.Tabbed
 import XMonad.Layout.WindowNavigation
@@ -53,9 +56,7 @@ myModMask         = mod4Mask
 myBorderWidth     = 3
 myHandleEventHook = fullscreenEventHook
 myManageHook      = composeAll [ className =? "feh" --> doCenterFloat ] <+>
-                    fullscreenManageHook <+>
-                    manageDocks <+>
-                    ( isFullscreen --> doFullFloat ) <+>
+                    fullscreenManageHook <+> manageDocks <+> ( isFullscreen --> doFullFloat ) <+>
                     manageHook defaultConfig
 myLayouts         = mkToggle (NOBORDERS ?? EOT) $ enableTabs $ fullscreenFull $
                     windowNavigation $ boringWindows $ smartBorders . avoidStruts $
@@ -68,8 +69,11 @@ myTabTheme = (theme wfarrTheme)
     }
 myWorkspaces      = ["www", "irc", "VMs", "4", "5", "6", "7", "8", "9"]
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-           [ ((modMask,               xK_a), sendMessage MirrorExpand)
+           [
+           -- Resizable Tiles
+             ((modMask,               xK_a), sendMessage MirrorExpand)
            , ((modMask,               xK_z), sendMessage MirrorShrink)
+           -- Tabbed tiles
            , ((modMask .|. controlMask, xK_h), sendMessage $ pullGroup L)
            , ((modMask .|. controlMask, xK_l), sendMessage $ pullGroup R)
            , ((modMask .|. controlMask, xK_k), sendMessage $ pullGroup U)
@@ -80,10 +84,13 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
            , ((modMask .|. controlMask, xK_period), onGroup W.focusDown')
            , ((modMask, xK_j), focusDown)
            , ((modMask, xK_k), focusUp)
-           , ((modMask .|. shiftMask, xK_h), spawn "feh --scale ~/Pictures/KB_United_States_Dvorak.svg.png")
-           , ((modMask .|. shiftMask, xK_slash), spawn "feh -B white --scale ~/Pictures/Xmbindings.png")
-           , ((modMask .|. shiftMask, xK_l), spawn "xscreensaver-command --lock")
+           -- Workspace controls
            , ((modMask .|. shiftMask, xK_b), sendMessage $ Toggle NOBORDERS)
+           -- Shortcuts
+           , ((modMask .|. shiftMask, xK_h),
+              spawn "feh --scale ~/Pictures/KB_United_States_Dvorak.svg.png")
+           , ((modMask .|. shiftMask, xK_slash),
+              spawn "feh -B white --scale ~/Pictures/Xmbindings.png")
+           , ((modMask .|. shiftMask, xK_l), spawn "xscreensaver-command --lock")
            ]
 newKeys x = myKeys x `M.union` keys defaultConfig x
-
