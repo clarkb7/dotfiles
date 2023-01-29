ef3c55c70c78df4a6e9cfc3c749afb5394fc337b
Branden Clark <branden.clark@datadoghq.com>
2023-01-28 23:02:40 -0500

    Initial commit

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,31 @@
+# http://onethingwell.org/post/586977440/mkcd-improved
+function mkcd
+{
+  dir="$*";
+  mkdir -p "$dir" && cd "$dir";
+}
+
+# Colored man pages
+# https://wiki.archlinux.org/index.php/Man_page#Colored_man_pages
+man() {
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[38;5;246m' \
+    LESS_TERMCAP_ue=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man "$@"
+}
+
+# grep
+grep_exclude_dirs=".git .tox .vs"
+grep_exclude_dir_opts=$(for d in $grep_exclude_dirs; do echo -n "--exclude-dir $d "; done)
+
+alias pygrep="grep -R --include '*.py'"
+alias gogrep="grep -R --include '*.go'"
+alias grep="grep $grep_exclude_dir_opts"
+alias rgrep="rgrep $grep_exclude_dir_opts"
+
+# cat with highlighting
+alias hl='highlight --style molokai --out-format=xterm256'
