fc0fbe1eac13c00ea6b80dc26e01c55ff51b9e6a
Branden Clark <branden.clark@datadoghq.com>
2023-02-07 12:15:30 -0500

    netstat

--- run_once_1-install-packages.sh
+++ run_once_1-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install vim git zip unzip tmux man-db htop wget highlight apt-file binutils file
+sudo apt install vim git zip unzip tmux man-db htop wget highlight apt-file binutils file net-tools
 sudo apt-file update
 
