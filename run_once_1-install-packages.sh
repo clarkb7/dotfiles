a54c35b521bba8e6487ea56e186d7065c7af1b0e
Branden Clark <branden.clark@datadoghq.com>
2023-02-06 20:22:32 -0500

    packages

--- run_once_1-install-packages.sh
+++ run_once_1-install-packages.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-sudo apt install vim git zip unzip tmux man-db htop wget highlight
+sudo apt update
+sudo apt install vim git zip unzip tmux man-db htop wget highlight apt-file binutils file
+sudo apt-file update
 
