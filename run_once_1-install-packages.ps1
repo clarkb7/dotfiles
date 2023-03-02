7682646e5a0ec6f00f015e217b78e4065d79be8d
Branden Clark <branden.clark@datadoghq.com>
2023-03-01 21:19:50 -0500

    install Golang

--- run_once_1-install-packages.ps1
+++ run_once_1-install-packages.ps1
@@ -1,3 +1,4 @@
 winget install -e --id Microsoft.Powershell --source winget
 winget install -e --id Git.Git --source winget
 winget install -e --id vim.vim --source winget
+winget install -e --id GoLang.Go.1.19
